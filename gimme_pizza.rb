1b549a134e3ed5dd5b17d150d69ad853d36663e1
ad3adp03t <ad3adp03t@gmail.com>
2017-07-06 14:48:58 -0400

    git pizza

--- gimme_pizza.rb
+++ gimme_pizza.rb
@@ -1,27 +1,79 @@
-size=["small","medium","large","x-large"]
-crust_style=["thin pan","hand tossed","deep dish","stuffed"]
-sauce=["robust tomato", "marinara", "alfredo"]
-sauce_portion=["light sauce","regular sauce","extra sauce"]
-cheese=["normal cheese","w/parmasean & romano","6 cheese blend"]
-meats=["peperonni","sausage","bacon","canadian bacon","chicken"]
-veggies=["mushrooms","olives","spinach","onions","garlic","Bananna peppers"]
+def size
+	size=["small","medium","large"].sample
+end
+def crust_style
+	crust_style=["thin crust","hand tossed crust","deep dish","stuffed crust"].sample
+end
+def sauce
+	sauce=["robust tomato", "marinara","alfredo"].sample
+end
+def sauce_portion
+	sauce_portion=["light sauce","regular sauce","extra sauce"].sample
+end
+def cheese
+	cheese=["standard mozerella","w/parmasean & romano cheese","6 cheese blend"].sample
+end
+def meats
+	meats=["peperonni","sausage","bacon","canadian bacon","chicken","Italian sausage","hamburger","steak"].sample
+end
+def veggies
+	veggies=["mushrooms","olives","spinach","onions","garlic","Bananna peppers"].sample
+end
+	
+def clock
+	clock=["20-35","36-45","46-59","60+"].sample
+end	
 
-p "Pizza's are $10 each. How many would you like?"
+def tip
+	if clock == "20-35"
+		tip = 5
+	elsif clock == "36-45"
+		tip = 3
+	elsif clock == "46-59"
+		tip = 2
+	else 
+		tip = 0
+	end	
+end	
+
+				
+
+p "How many pizza's would you like?"
 n=gets.chomp.to_i
 count = 1
+cost = 0
 n.times do
-puts "pizza #{count}"
-count+=1
-puts size.sample
-puts crust_style.sample
-puts sauce.sample
-puts sauce_portion.sample
-puts cheese.sample
-	if [1,2]==1
-		puts cheese.sample
-	else
-	end	
-puts meats.sample
-puts veggies.sample
+	puts "pizza #{count}"
+	pizza_size=size
+	count+=1
+	puts pizza_size
+	puts crust_style
+	puts sauce
+	puts sauce_portion
+	puts cheese
+	puts meats	
+	puts veggies
+	if pizza_size == "small"
+    	cost += 5
+    	puts "Current cost is $#{cost}"   
+	elsif pizza_size== "medium"
+    	cost += 8
+    	puts "Current cost is $#{cost}"
+	else pizza_size == "large"
+    	cost += 12
+    	puts "Current cost is $#{cost}"   
+	end   
+	puts " "
 end
-p "Your total is $#{n*10.60.to_f.round(2)}"
+
+
+puts "Do you want that delivered? There is a $2.50 delivery fee"
+deliv = gets.chomp
+	if deliv== "no"
+		puts "your total is $#{'%.2f' %(cost*0.06+cost)}"
+	elsif deliv== "yes"
+		puts "Your order arrived in #{clock} minutes. Your total is $#{'%.2f' %((cost*0.06+cost)+2.50+tip)}"
+	else
+		puts "Im sorry, I dont understand. Please try your order again."	
+	end	
+
